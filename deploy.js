d2a27761f4099f1d7dc585a296443916c661928f
shubhamcs <shubham.bagrecha@go-mmt.com>
2023-05-21 18:26:01 +0530

    create lottery contract

---
+++ deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+  "walk company crash mandate easily lemon wine sock apart ship antenna salt",
+  "https://sepolia.infura.io/v3/ea7a9f960a774bc28995ee16f435901e"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log("Attempting to deploy from account", accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode })
+    .send({ gas: "1000000", from: accounts[0] });
+
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
+deploy();
